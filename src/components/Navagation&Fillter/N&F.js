e7c92bb0466be4694445456de0224a5bb310043f
rogerpuig <rogerpuigdemasa@gmail.com>
2022-09-07 18:10:45 +0200

    filter and nav

---
+++ src/components/Navagation&Fillter/N&F.js
@@ -0,0 +1,31 @@
+import React, { useEffect } from 'react'
+import "./N&F.css"
+import Filter from "../../assets/img/Filter.png"
+
+export const NavAndFilter = ({actived , setActived,setModal,modal}) => {
+    
+    const handelClick = (e)=>{
+        setActived(e.target.innerHTML.toLowerCase());
+    }
+    let clasLatest = "selector latest"
+    let clasPopular = "selector popular"
+    if(actived === "latest"){
+        clasLatest += " activate"
+    }
+    else if(actived === "popular"){
+        clasPopular += " activate"
+    }
+
+    const filterModal = ()=>{
+        setModal(!modal)
+    }
+  return (
+    <div className='backNF'>
+        <div onClick={handelClick} className={clasLatest}>Latest</div>
+        <div onClick={handelClick} className={clasPopular}>Popular</div>
+        <div className='filter' onClick={filterModal}>
+            <img src={Filter}/>
+        </div>
+    </div>
+  )
+}
